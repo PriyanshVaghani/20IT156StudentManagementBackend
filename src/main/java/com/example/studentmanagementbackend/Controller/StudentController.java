79369a81a78dda2ba85c0f30ff2cc9676e1838f3
PriyanshVaghani <88379030+PriyanshVaghani@users.noreply.github.com>
2022-09-12 12:20:03 +0530

    Cors filer is added to handle axios request

--- src/main/java/com/example/studentmanagementbackend/Controller/StudentController.java
+++ src/main/java/com/example/studentmanagementbackend/Controller/StudentController.java
@@ -1,10 +1,18 @@
 package com.example.studentmanagementbackend.Controller;
 
 import com.example.studentmanagementbackend.Model.Student;
+import com.example.studentmanagementbackend.Payload.LoginRequest;
 import com.example.studentmanagementbackend.Repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @RestController
@@ -13,6 +21,9 @@ public class StudentController {
 
     @Autowired
     StudentRepository studentRepository;
+
+    @Autowired
+    AuthenticationManager authenticationManager;
 
     @GetMapping("/")
     public String displayWelcomeMessage() {
@@ -30,7 +41,7 @@ public class StudentController {
     }
 
     @PostMapping("/student")
-    public String  addStudent(@RequestBody Student student) {
+    public String addStudent(@RequestBody Student student) {
         boolean studentExists = studentRepository.existsById(student.getId());
         if (!studentExists) {
             studentRepository.save(student);
@@ -54,4 +65,27 @@ public class StudentController {
         studentRepository.save(studentObj);
         return studentRepository.findAll();
     }
+
+    @PostMapping("/login")
+    public String doLogin(@RequestBody LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(), loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return "Login Successful";
+    }
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+        System.out.println("Logout");
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "Logged out successful";
+    }
+
 }
